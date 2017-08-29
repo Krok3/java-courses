f8ebc9a7ba78f6719ff54ca9ab4780dda0f9f77f
krok3 <Vladyslav.Frundin@gmail.com>
2017-08-29 22:22:53 +0300

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,50 @@
 public class Calculate {
+
+	public static void calc(long first, long second, String operation) {
+		double rezult = 0;
+		if (operation .equals("+")) 
+			rezult = first + second;
+		else if (operation .equals("-"))
+			rezult = first - second;
+		else if (operation .equals("*"))
+			rezult = first * second;
+		else if (operation .equals(":") || operation .equals("/"))
+			rezult = (double)first / second;
+		else if (operation .equals("^"))
+			rezult = Math.pow(first, second);
+ 				
+		System.out.println(first + " " + operation + " " + second + " = " + rezult);   
+	} 
+	public static void calc(double first, double second, String operation) {
+		double rezult = 0;
+		if (operation .equals("+")) 
+			rezult = first + second;
+		else if (operation .equals("-"))
+			rezult = first - second;
+		else if (operation .equals("*"))
+			rezult = first * second;
+		else if (operation .equals(":") || operation .equals("/"))
+			rezult = first / second;
+		else if (operation .equals("^"))
+			rezult = Math.pow(first, second);
+ 				
+		System.out.println(first + " " + operation + " " + second + " = " + rezult);  
+	} 
+
 	public static void main(String[] arg) {
 		System.out.println("Calculate...");
-	} 
+                String operation = String.valueOf(arg[1]);
+
+		if (arg[0].indexOf(".") >= 0 || arg[2].indexOf(".") >= 0) {
+			double first = Double.valueOf(arg[0]);
+			double second = Double.valueOf(arg[2]);
+			calc(first, second, operation);
+		}
+		else	{
+			long first = Long.valueOf(arg[0]);
+			long second = Long.valueOf(arg[2]);
+			calc(first, second, operation);
+		}
+    }
+
 }
